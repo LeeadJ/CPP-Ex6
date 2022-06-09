f4890e6af0cf4c8ce7948f436035193e1f30ed6e
LeeadJ <leead123@gmail.com>
2022-06-09 15:10:57 +0300

    updated team

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -1,6 +1,19 @@
 #include "Team.hpp"
 
-Team::getName() const { return this->_name;}
+//Getters:
+std::string Team::getName() const { return this->_name;}
+double Team::getTalentLevel() const {return this->_talent_level;}
+int Team::getPointsScored() const {return this->_pointsScored;}
 
-Team::Team(std::string n) : _name(n);
-Team::Team(std::stringn, double lev) : _name(n), _talent_level(lev);
+//Setters:
+void Team::setPointsScored(int points) {this->_pointsScored=points;}
+
+//Constructors
+Team::Team(std::string n) : _name(n), _talent_level(-999), _pointsScored(0){}
+Team::Team(std::string n, double lev) : _name(n), _talent_level(lev){}
+
+//Operators:
+std::ostream& operator << (std::ostream& out, const Team& team){
+    out << "Team: " << team.getName() << ". Talent Level: " << team.getTalentLevel();
+    return out;
+}
--- sources/Team.hpp
+++ sources/Team.hpp
@@ -6,12 +6,21 @@ class Team{
     private:
         std::string _name;
         double _talent_level;
+        int _pointsScored;
 
     public:
         //Getters:
-        std:string getName() const;
+        std::string getName() const;
+        double getTalentLevel() const;
+        int getPointsScored() const;
+
+        //Setters:
+        void setPointsScored(int points);
         
         //Constructor:
         Team(const std::string n);
         Team(const std::string n, double lev);
+
+        //Operators:
+        friend std::ostream& operator << (std::ostream& out, const Team& team);
 };
