0f22402cdcad7d06796a045773c191ce00b2c757
LeeadJ <leead123@gmail.com>
2022-06-10 11:29:25 +0300

    friday update

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -9,8 +9,12 @@ int Team::getPointsScored() const {return this->_pointsScored;}
 void Team::setPointsScored(int points) {this->_pointsScored=points;}
 
 //Constructors
-Team::Team(std::string n) : _name(n), _talent_level(-999), _pointsScored(0){}
-Team::Team(std::string n, double lev) : _name(n), _talent_level(lev){}
+Team::Team(std::string n) : _name(n), _talent_level(0), _pointsScored(0){}
+Team::Team(std::string n, double lev) : _name(n), _talent_level(lev){
+    if(lev > 1.0 || lev < 0){
+        throw std::runtime_error("Team Error: Talent Level must be a double between 0 and 1.");
+    }
+}
 
 //Operators:
 std::ostream& operator << (std::ostream& out, const Team& team){
