c9407af0becf1992438c104bc9f0dc3eb779d4fd
LeeadJ <leead123@gmail.com>
2022-06-13 17:39:58 +0300

    team update

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -1,23 +1,47 @@
 #include "Team.hpp"
 
-//Getters:
-std::string Team::getName() const { return this->_name;}
-double Team::getTalentLevel() const {return this->_talent_level;}
-int Team::getPointsScored() const {return this->_pointsScored;}
-
-//Setters:
-void Team::setPointsScored(int points) {this->_pointsScored=points;}
-
 //Constructors
-Team::Team(std::string n) : _name(n), _talent_level(0), _pointsScored(0){}
+Team::Team(std::string n) : _name(n){
+    this->initTalentLevel();
+}
+
 Team::Team(std::string n, double lev) : _name(n), _talent_level(lev){
     if(lev > 1.0 || lev < 0){
         throw std::runtime_error("Team Error: Talent Level must be a double between 0 and 1.");
     }
+    this
 }
 
 //Operators:
 std::ostream& operator << (std::ostream& out, const Team& team){
     out << "Team: " << team.getName() << ". Talent Level: " << team.getTalentLevel();
     return out;
+}
+
+//Functions:
+void Team::initTalentLevel(){
+    double talent =((double)rand()/(double)RAND_MAX);
+    this->_talent_level=talent;
+}
+void Team::updateLWS(){
+    if(this->getCWS() > this->getLWS()){
+        this->setLWS(this->getCWS());
+    }
+    this->setCWS(0);
+}
+void Team::updateLLS(){
+    if(this->getCLS() > this->getLLS()){
+        this->setLLS(this->getCLS());
+    }
+    this->setCLS(0);
+}
+void Team::win(){
+    this->setWins(this->getWins()+1);
+    this->setCWS(this->getCWS()+1);
+    this->updateLLS();
+}
+void Team::loss(){
+    this->setLoss(this->getLosses()+1);
+    this->setCLS(this->getCLS()+1);
+    this->updateLWS();
 }
--- sources/Team.hpp
+++ sources/Team.hpp
@@ -5,22 +5,55 @@
 class Team{
     private:
         std::string _name;
-        double _talent_level;
+        double _talent_level=0;
         int _pointsScored;
+        int _wins=0;
+        int _losses=0;
+        int _pos_points = 0;
+        int _neg_points = 0;
+        int _CWS = 0;
+        int _CLS = 0;
+        int _LWS = 0;
+        int _LLS = 0;
 
     public:
         //Getters:
-        std::string getName() const;
-        double getTalentLevel() const;
-        int getPointsScored() const;
+        std::string getName() const {return this->_name;}
+        double getTalentLevel() const{return this->_talent_level;}
+        int getPointsScored() const{return this->_pointsScored;}
+        int getWins() const{return this->_wins;}
+        int getLosses() const{return this->_losses;}
+        int getPositvePoints() const{return this->_pos_points;}
+        int getNegativePoints() const{return this->_neg_points;}
+        int getCWS() const{return this->_CWS;}
+        int getCLS() const{return this->_CLS;}
+        int getLWS() const{return this->_LWS;}
+        int getLLS() const{return this->_LLS;}
+
 
         //Setters:
-        void setPointsScored(int points);
+        void setPointsScored(int points){this->_pointsScored=points;}
+        void setCWS(int num){this->_CWS=num;}
+        void setCLS(int num){this->_CLS=num;}
+        void setLWS(int num){this->_LWS=num;}
+        void setLLS(int num){this->_LLS=num;}
+        void setTalentLevel(double lev){this->_talent_level=lev;}
+        void setWins(int num) {this->_wins=num;}
+        void setLoss(int num) {this->_losses=num;}
         
+        
+
         //Constructor:
         Team(const std::string n);
         Team(const std::string n, double lev);
 
         //Operators:
         friend std::ostream& operator << (std::ostream& out, const Team& team);
+
+        //Functions:
+        void updateLWS();
+        void updateLLS();
+        void initTalentLevel();
+        void win();
+        void loss();
 };
