d9a5d64966234162eea3e832709bdadcaf9579d7
LeeadJ <leead123@gmail.com>
2022-06-15 12:38:49 +0300

    updated test + uploaded doctest

--- Test.cpp
+++ Test.cpp
@@ -1,3 +1,4 @@
+#include "doctest.h"
 #include <iostream>
 #include "sources/Game.hpp"
 #include "sources/Team.hpp"
@@ -8,19 +9,21 @@ using namespace std;
 TEST_CASE("Default Tests"){
     Team a{"sixers", 0.1121};
     Team b{"knicks", 0.4};
-    CHECK(a.getName()=="sixers")
-    CHECK(b.getName()=="knicks")
+    CHECK(a.getName()=="sixers");
+    CHECK(b.getName()=="knicks");
     League league{};
-    CHECK(league.teams->size() == 20);
-    CHECK(league.getLeagueVec().at(1)=="Celtics");
-    CHECK(League.league_LWS()==0);
+    CHECK_EQ(league.getLeagueVec().size(), 20);
+    CHECK_EQ(league.getLeagueVec().at(1), "Celtics");
+    CHECK_EQ(league.league_LWS(), 0);
+
     Schedule schedule(league);
-    CHECK(schedule.rounds == 38);
-    CHECK(schedule.num_teams == 20);    
-    schedule.run_the_season();
-    CHECK(schedule._league.league_LWS().getLWS() != 0);
-    CHECK(schedule._league.league_LLS().getLLS() != 0);
-    CHECK(schedule._league.league_MostPoints().getTotalPoints() != 0);
-    CHECK(schedule._league.teamWithFewestPoints().getTotalPoints() != 0);
-    CHECK(schedule._league.numPosTeams() != 0);
+    CHECK_EQ(schedule.getRounds(), 38);
+    CHECK_EQ(schedule.getNumTeams(), 20); 
+
+    schedule.start_season();
+    CHECK_FALSE(schedule._league->league_LWS()==0);
+    CHECK_FALSE(schedule._league->league_LLS()->getLLS() == 0);
+    CHECK_FALSE(schedule._league->league_MostPoints()->getTotalPoints() == 0);
+    CHECK_FALSE(schedule._league->teamWithFewestPoints()->getTotalPoints() == 0);
+    CHECK_FALSE(schedule._league->numPosTeams() != 0);
 }
